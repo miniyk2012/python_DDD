9570c6010e94e83dbe8557cc102e757cc4d2a0f5
杨恺 <yangkai.04@bytedance.com>
2021-06-07 18:24:42 +0800

    [test] 依赖反转的一个优美例子, 但是写起来确实很复杂

--- sync.py
+++ sync.py
@@ -5,14 +5,14 @@ from pathlib import Path
 
 
 def sync(source, dest):
-    # imperative shell step 1, gather inputs
+    # imperative shell step 1, gather inputs, 命令式的
     source_hashes = read_paths_and_hashes(source)
     dest_hashes = read_paths_and_hashes(dest)
 
-    # step 2: call functional core
+    # step 2: call functional core, 这是核心逻辑
     actions = determine_actions(source_hashes, dest_hashes, source, dest)
 
-    # imperative shell step 3, apply outputs
+    # imperative shell step 3, apply outputs, 命令式的
     for action, *paths in actions:
         if action == "COPY":
             shutil.copyfile(*paths)
@@ -20,6 +20,21 @@ def sync(source, dest):
             shutil.move(*paths)
         if action == "DELETE":
             os.remove(paths[0])
+
+
+# 所有的外部依赖都通过参数注入
+def synchronise_dirs(reader, filesystem, filesystem2, source_root, dest_root):
+    source_hashes = reader(source_root)
+    dest_hashes = reader(dest_root)
+
+    actions = determine_actions(source_hashes, dest_hashes, source_root, dest_root)
+    for action, *paths in actions:
+        if action == "COPY":
+            filesystem.copyfile(*paths)
+        if action == "MOVE":
+            filesystem.move(*paths)
+        if action == "DELETE":
+            filesystem2.remove(paths[0])
 
 
 BLOCKSIZE = 65536
@@ -57,4 +72,4 @@ def determine_actions(source_hashes, dest_hashes, source_folder, dest_folder):
 
     for sha, filename in dest_hashes.items():
         if sha not in source_hashes:
-            yield "DELETE", dest_folder / filename
+            yield "DELETE", Path(dest_folder) / Path(filename)
--- test_sync.py
+++ test_sync.py
@@ -1,10 +1,97 @@
 import tempfile
+import os
 from pathlib import Path
 import shutil
-from sync import sync, determine_actions
+from sync import sync, determine_actions, synchronise_dirs, read_paths_and_hashes
+
+
+class FakeFileSystem(list):
+
+    def copyfile(self, src, dest):
+        self.append(('COPY', str(src), str(dest)))
+
+    def move(self, src, dest):
+        self.append(('MOVE', str(src), str(dest)))
+
+    def remove(self, dest):
+        self.append(('DELETE', str(dest)))
+
+
+class TestDiE2E:
+    """Test 依赖反转的End to End"""
+
+    @staticmethod
+    def test_fake_di_sync_copy():
+        source = {"sha1": "my-file"}
+        dest = {}
+        filesystem = FakeFileSystem()
+        reader = {"/source": source, "/dest": dest}
+        synchronise_dirs(reader.pop, filesystem, filesystem, "/source", "/dest")
+        assert filesystem == [("COPY", "/source/my-file", "/dest/my-file")]
+
+    @staticmethod
+    def test_fake_di_sync_delete():
+        source = {}
+        dest = {"sha1": "my-file"}
+        filesystem = FakeFileSystem()
+        reader = {"/source": source, "/dest": dest}
+        synchronise_dirs(reader.pop, filesystem, filesystem, "/source", "/dest")
+        assert filesystem == [('DELETE', '/dest/my-file')]
+
+    @staticmethod
+    def test_fake_di_sync_move():
+        source = {"sha1": "my-file", "sha2": "my-file2"}
+        dest = {"sha1": "my-file", "sha2": "my-file3"}
+        filesystem = FakeFileSystem()
+        reader = {"/source": source, "/dest": dest}
+        synchronise_dirs(reader.pop, filesystem, filesystem, "/source", "/dest")
+        assert filesystem == [('MOVE', '/dest/my-file3', '/dest/my-file2')]
+
+    @staticmethod
+    def test_di_sync_when_a_file_exists_in_the_source_but_not_the_destination():
+        try:
+            source = tempfile.mkdtemp()
+            dest = tempfile.mkdtemp()
+
+            content = "I am a very useful file"
+            (Path(source) / "my-file").write_text(content)
+
+            synchronise_dirs(read_paths_and_hashes, shutil, os, source, dest)
+
+            expected_path = Path(dest) / "my-file"
+            assert expected_path.exists()
+            assert expected_path.read_text() == content
+
+        finally:
+            shutil.rmtree(source)
+            shutil.rmtree(dest)
+
+    @staticmethod
+    def test_di_sync_when_a_file_has_been_renamed_in_the_source():
+        try:
+            source = tempfile.mkdtemp()
+            dest = tempfile.mkdtemp()
+
+            content = "I am a file that was renamed"
+            source_path = Path(source) / "source-filename"
+            old_dest_path = Path(dest) / "dest-filename"
+            expected_dest_path = Path(dest) / "source-filename"
+            source_path.write_text(content)
+            old_dest_path.write_text(content)
+
+            synchronise_dirs(read_paths_and_hashes, shutil, os, source, dest)
+
+            assert old_dest_path.exists() is False
+            assert expected_dest_path.read_text() == content
+
+        finally:
+            shutil.rmtree(source)
+            shutil.rmtree(dest)
 
 
 class TestE2E:
+    """Test End to End"""
+
     @staticmethod
     def test_when_a_file_exists_in_the_source_but_not_the_destination():
         try:
