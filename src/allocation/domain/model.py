f7068ba397ced95d27db275288a424187c1ec67b
yangkai.04 <yangkai.04@bytedance.com>
2021-08-04 09:50:37 +0800

    [feat] gitignore

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -11,14 +11,16 @@ class OutOfStock(Exception):
 class Product:
     """ dummy implementation, fixme"""
 
-    def __init__(self, sku, batches):
+    def __init__(self, sku, batches, version_number: int = 0):
         self.sku = sku  # type: str
         self.batches = batches  # type: List[Batch]
+        self.version_number = version_number
 
     def allocate(self, line):
         try:
             batch = next(b for b in sorted(self.batches) if b.can_allocate(line))
             batch.allocate(line)
+            self.version_number += 1
             return batch.reference
         except StopIteration:
             raise OutOfStock(f"Out of stock for sku {line.sku}")
